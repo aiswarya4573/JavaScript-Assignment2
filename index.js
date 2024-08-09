588f58a4fd1184fffa6875935ec39992712306df
aiswarya4573 <aiswarya457373@gmail.com>
2024-08-09 17:49:04 +0530

    modal and add to cart done

--- index.js
+++ index.js
@@ -37,3 +37,53 @@ function closeModal(e, clickedOutside) {
 openBtn.addEventListener("click", openModal);
 modal.addEventListener("click", (e) => closeModal(e, true));
 closeBtn.addEventListener("click", closeModal);
+
+
+const item = document.querySelectorAll(".price-content,.content,.img-outer-container,.first,.second,.third,.fourth,.fifth,.sixth,.seventh,.eigth,.nineth,.tenth,.eleventh,.twelveth,.thirteenth,.fourteenth,.fifteenth,.sixteenth,.seventeenth,.eighteenth,.nineteenth,.twenty");
+const modalImage = document.querySelector(".modal-img");
+const modalName = document.querySelector(".modal-name");
+const modalPrice = document.querySelector(".modal-price");
+
+
+item.forEach((items) => {
+
+    items.addEventListener("click", () =>{
+        openModal();
+        modalImage.src = items.querySelector(".image").src;
+        modalName.textContent=items.querySelector(".name").textContent;
+        modalPrice.textContent=items.querySelector(".price").textContent;
+    })
+})
+
+
+
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const quantity = document.querySelector(".quantity-no");
+const cartNo = document.querySelector(".cart-no");
+let count = 0;
+
+plus.addEventListener("click", () => {
+    count = count + 1;
+    quantity.textContent=count;
+    cartNo.textContent=count;
+
+
+
+
+})
+
+minus.addEventListener("click", () => {
+    count = count - 1;
+    quantity.textContent=count;
+    cartNo.textContent=count;
+
+
+
+
+})
+
+
+    
+
+
